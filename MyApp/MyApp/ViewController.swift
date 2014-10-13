cf1624f55731e85268f0c40127f0e93a954e66db
Michael Moser <mike@hugravi.com>
2014-10-13 10:44:40 -0700

    Add an application project (not yet using Framework);

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Michael Moser on 10/13/14.
+//  Copyright (c) 2014 Michael Moser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
