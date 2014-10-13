4ab78a63a691fcb4246eebd7fe1593e0050c357f
Michael Moser <mike@hugravi.com>
2014-10-13 10:38:44 -0700

    Create Cocoa Touch Framework for Swift.

---
+++ MySwiftFramework/MySwiftFramework/MySwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  MySwiftFramework.h
+//  MySwiftFramework
+//
+//  Created by Michael Moser on 10/13/14.
+//  Copyright (c) 2014 Michael Moser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySwiftFramework.
+FOUNDATION_EXPORT double MySwiftFrameworkVersionNumber;
+
+//! Project version string for MySwiftFramework.
+FOUNDATION_EXPORT const unsigned char MySwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySwiftFramework/PublicHeader.h>
+
+
